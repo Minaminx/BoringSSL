115080cdd1907ec4ff7faeb53de22753eacc35f0
NewBugger <contacts@sometimesnaive.org>
2018-08-24 21:41:31 +0800

    bugfix: error: suggest parenttheses around assignment used as truth value
    even more common id for people to use '=' when they mean '=='.

--- ssl/handshake_client.cc
+++ ssl/handshake_client.cc
@@ -230,7 +230,7 @@ static int ssl_write_client_cipher_list(SSL_HANDSHAKE *hs, CBB *out) {
 
   // Add TLS 1.3 ciphers. Order ChaCha20-Poly1305 relative to AES-GCM based on
   // hardware support.
-  if (hs->max_version = TLS1_3_VERSION) {
+  if (hs->max_version == TLS1_3_VERSION) {
     /// if (!EVP_has_aes_hardware() &&
     ///     !CBB_add_u16(&child, TLS1_CK_CHACHA20_POLY1305_SHA256 & 0xffff)) {
     ///   return 0;
